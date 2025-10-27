9e4d06a93f162b94bd4f84077a9c688728e2784f
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2025-10-27 17:38:02 -0400

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,4 +2,21 @@ package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase {}
+public class Shooter extends SubsystemBase {
+
+    private final ShooterIO shooterIO;
+    private final ShooterIOInputsAutoLogged shooterInputs = new ShooterIOInputsAutoLogged();
+    
+    public Shooter(ShooterIO io) {
+        this.shooterIO = io;
+    }
+
+    @Override
+    public void periodic() {
+        shooterIO.updateInputs(shooterInputs);
+    }
+
+    
+
+
+}
