7df993cc3b0a262bb954ed2d3398a95e95bb6597
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2025-10-28 16:01:42 -0400

    Quick dumb change

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
@@ -1,3 +1,50 @@
 package frc.robot.subsystems.shooter;
 
-public class ShooterIOSparkMax implements ShooterIO {}
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.EncoderConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import frc.robot.subsystems.shooter.ShooterConstants.HardwareConstants;
+
+
+public class ShooterIOSparkMax implements ShooterIO {
+
+    private SparkFlex topMotor; 
+    private SparkFlex bottomMotor;
+
+    private RelativeEncoder encoder;
+
+    private PIDController feedbackController;
+    private SimpleMotorFeedforward feedForwardController = new SimpleMotorFeedforward(0.0,0.0,0.0);
+
+
+    public ShooterIOSparkMax() {
+        topMotor = new SparkFlex(HardwareConstants.TOP_MOTOR_ID, MotorType.kBrushless);
+        bottomMotor = new SparkFlex(HardwareConstants.BOTTOM_MOTOR_ID, MotorType.kBrushless);
+
+        encoder = topMotor.getEncoder();
+
+        topMotor.configure(
+            new SparkFlexConfig()
+                .smartCurrentLimit(0)
+                .voltageCompensation(0.0)
+                .idleMode(IdleMode.kBrake)
+                .inverted(false)
+                .apply(
+                    new EncoderConfig()
+                        .velocityConversionFactor(0)
+                        .positionConversionFactor(0)
+                ), 
+            ResetMode.kResetSafeParameters,
+            PersistMode.kPersistParameters);
+    }
+
+    
+}
