25d11313d015069e85ccdd73f5dc43720f3a666a
Ning-NJ <ning.y07090@gmail.com>
2025-11-02 10:18:37 -0500

    did some commands

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.shooter;
 
 import static frc.robot.subsystems.shooter.ShooterConstants.*;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.DoubleSupplier;
 
@@ -12,7 +12,13 @@ public class Shooter extends SubsystemBase {
   public Shooter(ShooterIO io) {
     this.io = io;
   }
+  public void periodic() {}
 
-  public Command runVoltage(DoubleSupplier volts) {
+  public Command runVoltage(DoubleSupplier leftVoltage, DoubleSupplier rightVoltage) {
+    return new RunCommand(() -> io.setVoltage(leftVoltage.getAsDouble(), rightVoltage.getAsDouble()));
+  }
+
+  public Command runRPM(DoubleSupplier leftRpm, DoubleSupplier rightRpm) {
+    return new RunCommand(() -> io.setRPM(leftRpm.getAsDouble(), rightRpm.getAsDouble()));
   }
 }
