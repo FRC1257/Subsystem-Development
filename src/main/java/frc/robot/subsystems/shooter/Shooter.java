c7b577f672892ca81b0be9f2c5e172813ae8f5f6
Caitlin-78 <caitlinsayah@gmail.com>
2025-11-03 12:06:59 -0500

    added run commands to shooter.java (These were in last commit but got left out for some reason)

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,11 +1,20 @@
-package src.main.java.frc.robot.subsystems.shooter;
+package frc.robot.subsystems.shooter;
 
-public class shooter extends SubsystemBase {
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    public ShooterIO ShooterIO;
     public Shooter(ShooterIO io){
-        this.io = io;
+            ShooterIO = io;
     }
 
-    //will finish commands here later
-    public Command runVoltage(DoubleSupplier voltage){}
-    public Command runRPM(DoubleSupplier rpm){}
+    public Command runVoltage(DoubleSupplier voltage){
+        return new RunCommand(()->ShooterIO.setVoltage(voltage.getAsDouble()), this).withName("Shooter Voltage");
+    }
+    public Command runRPM(DoubleSupplier rpm){
+        return new RunCommand(()->ShooterIO.setRPM(rpm.getAsDouble()), this).withName("Shooter RPM");
+    }
 }
