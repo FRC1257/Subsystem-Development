430c1854a46322b434e8e08aebd1c89aece59602
Sam Beilis <78675126+ipad6202@users.noreply.github.com>
2025-11-13 16:01:30 -0500

    Commit 2

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.arm;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ArmIO {
-  
+
   @AutoLog
   public class ArmIOInputs {
     public double angleRads = 0.0;
---
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
@@ -0,0 +1,63 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+public class ArmIOSim implements ArmIO {
+
+    private final DCMotor m_armGearbox = DCMotor.getNEO(1);
+    private final ProfiledPIDController m_controller;
+
+
+    private SingleJointedArmSim sim =
+      new SingleJointedArmSim(
+          m_armGearbox,
+          CoralPivotConstants.CoralPivotSimConstants.kArmReduction,
+          SingleJointedArmSim.estimateMOI(
+              CoralPivotConstants.CoralPivotSimConstants.kArmLength,
+              CoralPivotConstants.CoralPivotSimConstants.kArmMass),
+          CoralPivotConstants.CoralPivotSimConstants.kArmLength,
+          CoralPivotConstants.CoralPivotSimConstants.kMinAngleRads,
+          CoralPivotConstants.CoralPivotSimConstants.kMaxAngleRads,
+          true,
+          0.1);
+
+
+    public ArmIOSim() {
+
+         m_controller =
+            new ProfiledPIDController(
+                CoralPivotConstants.CoralPivotSimConstants.kPivotSimPID[0],
+                CoralPivotConstants.CoralPivotSimConstants.kPivotSimPID[1],
+                CoralPivotConstants.CoralPivotSimConstants.kPivotSimPID[2],
+                new TrapezoidProfile.Constraints(2.85, 15));
+        m_controller.setTolerance(0.1, 0.05);
+
+    }
+
+    @Override
+    public void updateInputs(ArmIOInputs inputs) {
+        sim.update(0.02);
+        inputs.angVelocityRadsPerSec = sim.getVelocityRadPerSec();
+        inputs.angleRads = sim.getAngleRads();
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        sim.setInputVoltage(voltage); 
+    }
+
+    @Override
+    public void setPosition(double angleRads) {
+        double pidOutput = m_controller.calculate(sim.getAngleRads(), angleRads); 
+        sim.setInputVoltage(MathUtil.clamp(pidOutput, -12, 12)); 
+    }
+
+    @Override
+    public void stop() {
+        sim.setInputVoltage(0.0);
+    }
+}
