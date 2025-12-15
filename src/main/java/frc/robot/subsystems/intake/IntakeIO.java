93317b3c148242a6ecd145e926bb583d983f3e37
Ning-NJ <ning.y07090@gmail.com>
2025-12-15 15:34:17 -0500

    Update IntakeIO.java
    update

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,3 +1,21 @@
+import org.littletonrobotics.junction.AutoLog;
+
 public class IntakeIO {
-    
+    public static class IntakeIOInputs {
+    public double velocityRadsPerSec = 0.0;
+
+    public double appliedVoltage = 0.0;
+    public double[] currentAmps = new double[] {};
+    public double[] tempCelcius = new double[] {};
+  }
+  public void setBrake(boolean brake) {}
+
+  public void updateInputs(IntakeIOInputs inputs) {}
+
+  public void setVoltage(double voltage) {}
+
+  public double getVelocity() {
+    return 0;
+  }
+  public void setDirection(boolean forward){}
 }
