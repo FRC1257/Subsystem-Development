b22d7a391ed5bcdcd2b2f1d65fe4433b0a7a24d2
Caitlin-78 <caitlinsayah@gmail.com>
2025-12-15 16:36:17 -0500

    fixed errors from missing semicolons

--- src/main/java/frc/robot/subsystems/dropper/DropperConstants.java
+++ src/main/java/frc/robot/subsystems/dropper/DropperConstants.java
@@ -1,13 +1,15 @@
 package frc.robot.subsystems.dropper;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+
 public class DropperConstants {
     
     public static Constraints kDropperConstraints;
     public static final double UPPER_LIMIT = 1;
     public static final double LOWER_LIMIT = -1;
 
-    public static final int LEFT_MOTOR_ID = 0
-    public static final int RIGHT_MOTOR_ID = 0
+    public static final int LEFT_MOTOR_ID = 0;
+    public static final int RIGHT_MOTOR_ID = 0;
 
 
 }
