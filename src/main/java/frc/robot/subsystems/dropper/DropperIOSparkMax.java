e3288a887beeecaf9a7384bf81836b464350e001
laelHI <lgittens@ucvts.org>
2025-12-15 16:54:20 -0500

    SparkMax initial code

--- src/main/java/frc/robot/subsystems/dropper/DropperIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/dropper/DropperIOSparkMax.java
@@ -1,5 +1,35 @@
 package frc.robot.subsystems.dropper;
 
-public class DropperIOSparkMax extends DropperIO {
+import static frc.robot.Constants.NEO_CURRENT_LIMIT;
+
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+public class DropperIOSparkMax implements DropperIO {
     
+    private ProfiledPIDController controller = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile(-12, 12));
+    private SparkFlex motor;
+    private RelativeEncoder encoder;
+
+    public DropperIOSparkMax() {
+        leftmotor = new SparkFlex(DropperConstants.blank, MotorType.kBrushless);//make a constant for motor id and ask the type of motor
+        rightmotor = new SparkFlex(DropperConstants.blankmotorid, MotorType.kbrushless);
+
+        leftEncoder = leftMotor.getEncoder();
+        rightEncoder = rightMotor.getEncoder();
+
+        SparkFlexConfig config = new SparkFlexConfig();
+        config
+            .idleMode(IdleMode.kbrake)
+            .voltageCompensation(12)
+            .smartCurrentLimit(NEO_CURRENT_LIMIT)
+            .inverted(true);
+        
+        leftMo
+
+
+        
+    }
+
+
+
 }
