9b6b3eb8d0516213aaf264c27b74480c22ab926e
greenbeen2906 <gchakravarty@ucvts.org>
2025-12-15 16:27:35 -0500

    IO and Constants

--- src/main/java/frc/robot/subsystems/dropper/DropperConstants.java
+++ src/main/java/frc/robot/subsystems/dropper/DropperConstants.java
@@ -2,9 +2,12 @@ package frc.robot.subsystems.dropper;
 
 public class DropperConstants {
     
-    public static Constraints kArmConstraints;
+    public static Constraints kDropperConstraints;
     public static final double UPPER_LIMIT = 1;
     public static final double LOWER_LIMIT = -1;
 
-    // add motor id's
+    public static final int LEFT_MOTOR_ID = 0
+    public static final int RIGHT_MOTOR_ID = 0
+
+
 }
