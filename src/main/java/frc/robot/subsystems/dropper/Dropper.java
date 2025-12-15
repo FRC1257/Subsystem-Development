2715dbd81b603ea099cea10ef60fd86f1dd5aa10
Caitlin-78 <caitlinsayah@gmail.com>
2025-12-15 16:26:19 -0500

    Made dropper.java main commands

--- src/main/java/frc/robot/subsystems/dropper/Dropper.java
+++ src/main/java/frc/robot/subsystems/dropper/Dropper.java
@@ -1,28 +1,37 @@
 package frc.robot.subsystems.dropper;
 
-import org.littletonrobotics.junction.Logger;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.util.function.DoubleSupplier;
+
+import org.littletonrobotics.junction.Logger;
+
 public class Dropper extends SubsystemBase {
-    private final DropperIO io;
-    private final DropperIOInputsAutoLogged inputs = new DropperIOInputsAutoLogged();
-    
-    public Dropper(DropperIO io) {
-        this.io = io;
-    }
+  private final DropperIO io;
+  private final DropperIOInputsAutoLogged inputs = new DropperIOInputsAutoLogged();
 
-    @Override
-    public void periodic() {
-        io.updateInputs(inputs);
-        Logger.processInputs("Dropper", inputs);
-    }
+  public Dropper(DropperIO io) {
+    this.io = io;
+  }
 
-    public command stop(){
-        return runOnce(() -> io.stop().withName("Dropper Stop"));
-    }
-    
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Dropper", inputs);
+  }
 
+  public Command runVoltage(DoubleSupplier voltage) {
+    return new RunCommand (() -> io.setVoltage(voltage), this).withName("Dropper Voltage");
+  }
 
 
+  public Command stop() {
+    return runOnce(() -> io.stop().withName("Dropper Stop"));
+  }
+
+  public void setPIDGains(double kP, double kI, double kD) {
+    io.setPIDGains(kP, kI, kD);
+  }
 }
