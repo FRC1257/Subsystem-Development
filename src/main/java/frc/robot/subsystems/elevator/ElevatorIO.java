d175463431c6510b6d5b2a5810f9438ce92fbba3
Arman1291 <arman.aliyev.2009@gmail.com>
2025-12-15 16:51:42 -0500

    Sparkmax 80% constants some work

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -8,25 +8,60 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
-//motor is neos
-public interface ElevatorIO {
-    @AutoLog
+//motor is neos, we still need motor methods
 
-     public static class ElevatorIOInputs {
-    //public Voltage 
-    public boolean limitSwitchPressed = false;
+public interface ElevatorIO {
+    // Hardware API (Used by SparkMax)
+    
+    
+    @AutoLog
+    public static class ElevatorIOInputs {
+        public double setpointMeters = 0.0;
+        public double positionMeters = 0.0;
+        public double velocityMetersPerSec = 0.0;
+        public double appliedVoltage = 0.0;
+        public boolean limitSwitchPressed = false;
      }
 
-     default void setVoltage(Double Voltage){}
-     default double getVoltage() {
-        return 67.5;
-    }
-    ProfiledPIDController controller = new ProfiledPIDController(
-  kP, kI, kD,
-  new TrapezoidProfile.Constraints(MaxVelocity, MaxAcceleration));
-
+    
     default void updateInputs(ElevatorIOInputs inputs) {}
-    default void setPIDGains(double kP, double kI, double kD) {}
-    default void setRPM(double rpm){}
-    default void setFeedForwardGains(double kS, double kV, double kA) {}
-}
+
+    // Getter method for target height
+    default double getSetpoint() {
+        return 0.0;
+    }
+   
+    // Getter method for current position
+    default double getPosition() {
+        return 0.0;
+    }
+
+    // Getter method for velocity
+    default double getVelocity(){
+        return 0.0;
+    }
+
+    // Checking if elevator goes above limit height
+    default boolean isLimitSwitchPressed() {
+        return false;
+    }
+
+
+    // Subsystem API (Used by commands)
+   
+    
+    // The target height in meters
+    default void setSetpoint(double setpoint) {}
+
+    // Profiled PID to go to setpoint
+    public default void goToSetpoint() {}
+
+    // Returns true if elevator at setpoint
+    public default boolean atSetpoint() {
+        return false;
+      }
+
+    // Setter for voltage 
+    default void setVoltage(double voltage) {}
+
+    }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSparkMax.java
@@ -1,8 +1,220 @@
 package frc.robot.subsystems.elevator;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+import org.littletonrobotics.junction.Logger;
 
-public class ElevatorIOSparkMax {
+public class ElevatorIOSparkMax implements ElevatorIO {
+
+     double lastTime = Timer.getFPGATimestamp();
+
+private SparkMax leftMotor;
+  // right follows left
+  private SparkMax rightMotor;
+  private RelativeEncoder leftEncoder;
+
+
      private ProfiledPIDController pidController =
       new ProfiledPIDController(
+          ElevatorConstants.ELEVATOR_REAL_PID[0],
+          ElevatorConstants.ELEVATOR_REAL_PID[1],
+          ElevatorConstants.ELEVATOR_REAL_PID[2],
+          new TrapezoidProfile.Constraints(
+              ElevatorConstants.MAX_VELOCITY, ElevatorConstants.MAX_ACCELERATION));
+
+              
+  public ElevatorIOSparkMax() {
+    leftMotor = new SparkMax(ElevatorConstants.LEFT_MOTOR_ID, MotorType.kBrushless);
+    rightMotor = new SparkMax(ElevatorConstants.RIGHT_MOTOR_ID, MotorType.kBrushless);
+
+    leftEncoder = leftMotor.getEncoder();
+    leftEncoder.setPosition(0);
+
+    SparkMaxConfig leftConfig = new SparkMaxConfig();
+    leftConfig
+        .smartCurrentLimit(Constants.NEO_CURRENT_LIMIT)
+        .idleMode(ElevatorConstants.MOTOR_DEFAULT_IDLE_MODE) //??
+        .voltageCompensation(12.0)
+        .inverted(false);
+    leftConfig
+        .encoder
+        .positionConversionFactor(ElevatorConstants.POSITION_CONVERSION_FACTOR) 
+        .velocityConversionFactor(ElevatorConstants.POSITION_CONVERSION_FACTOR / 60.0);
+
+    SparkMaxConfig rightConfig = new SparkMaxConfig();
+    rightConfig.apply(leftConfig);
+    rightConfig.follow(leftMotor, true);
+
+    // reset safe kResetSafeParameters switches the motor to default paramaters, then adds the
+    // changes from the config object
+    // persist paramaters saves these changes to the motor memory so it doesn't get cooked during
+    // brownouts
+    // only use persist in the BEGINNING, not later
+    leftMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightMotor.configure(
+        rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    pidController.setTolerance(ElevatorConstants.SETPOINT_TOLERANCE_METERS);
+
+    limitSwitch = new DigitalInput(ElevatorConstants.LIMIT_SWITCH_CHANNEL);
+
+  }
+
+  private void updateMotorConfig(SparkMaxConfig config) {
+    // DO NOT RESET paramaters becasue we only want to change some paramaters, not all
+    // DO NOT PERSIST because this is a temporary change that we don't want to save to memory
+    leftMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    rightMotor.configure(
+        config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+  }
+
+  
+
+  @Override
+  public double getSetpoint() {
+    return pidController.getSetpoint().position;
+  }
+
+  @Override
+  public double getPosition() {
+    // get the absolute position in radians, then convert to meters
+    return leftEncoder.getPosition();
+  }
+
+  @Override
+  public void setSetpoint(double setpoint) {
+    pidController.setGoal(setpoint);
+    pidController.reset(getPosition(), getVelocity());
+  }
+
+  //@Override
+//   public void goToSetpoint() { ///////////////needs fixing
+//     double pidOutput = pidController.calculate(getPosition());
+
+//     // change in velocity / change in time = acceleration
+//     // Acceleration is used to calculate feedforward
+//     double acceleration =
+//         (pidController.getSetpoint().velocity - lastSpeed) / (Timer.getFPGATimestamp() - lastTime);
+
+//     double ffOutput = feedforward.calculate(pidController.getSetpoint().velocity, acceleration);
+
+//     setVoltage(MathUtil.clamp(pidOutput + ffOutput, -12, 12));
+
+//     lastSpeed = pidController.getSetpoint().velocity;
+//     lastTime = Timer.getFPGATimestamp();
+//   }
+
+  @Override
+  public boolean atSetpoint() {
+    // if the difference between setpoint and position is less than the tolerance
+    return pidController.atGoal();
+  }
+
+  @Override
+  public double getVelocity() {
+    return leftEncoder.getVelocity();
+  }
+
+  @Override
+  public void setVoltage(double voltage) {
+    leftMotor.setVoltage(voltage);
+    Logger.recordOutput("Elevator/Desired Voltage", voltage);
+  }
+
+  @Override
+  public void setBrakeMode(boolean brakeEnabled) {
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(brakeEnabled ? IdleMode.kBrake : IdleMode.kCoast);
+    updateMotorConfig(config);
+  }
+
+//   @Override
+//   public boolean isLimitSwitchPressed() {
+//     return !limitSwitch.get();
+//     // return false;
+//   }
+
+  @Override
+  public double getP() {
+    return pidController.getP();
+  }
+
+  @Override
+  public double getI() {
+    return pidController.getI();
+  }
+
+  @Override
+  public double getD() {
+    return pidController.getD();
+  }
+
+  @Override
+  public double getkS() {
+    return feedforward.getKs();
+  }
+
+  @Override
+  public double getkG() {
+    return feedforward.getKg();
+  }
+
+  @Override
+  public double getkV() {
+    return feedforward.getKv();
+  }
+
+  @Override
+  public double getkA() {
+    return feedforward.getKa();
+  }
+
+  @Override
+  public void setP(double kP) {
+    pidController.setP(kP);
+  }
+
+  @Override
+  public void setI(double kI) {
+    pidController.setI(kI);
+  }
+
+  @Override
+  public void setD(double kD) {
+    pidController.setD(kD);
+  }
+
+  @Override
+  public void setkS(double kS) {
+    feedforward.setKs(kS);
+  }
+
+  @Override
+  public void setkG(double kG) {
+    feedforward.setKg(kG);
+  }
+
+  @Override
+  public void setkV(double kV) {
+    feedforward.setKv(kV);
+  }
+
+  @Override
+  public void setkA(double kA) {
+    feedforward.setKa(kA);
+  }
 }
+
+
