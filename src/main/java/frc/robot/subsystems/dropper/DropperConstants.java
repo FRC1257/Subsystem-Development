a26c02ba0f392359fdef55e62fec22e4d3b7ade7
greenbeen2906 <gchakravarty@ucvts.org>
2025-12-15 15:57:11 -0500

    IO.java and Constants

--- src/main/java/frc/robot/subsystems/dropper/DropperConstants.java
+++ src/main/java/frc/robot/subsystems/dropper/DropperConstants.java
@@ -2,4 +2,9 @@ package frc.robot.subsystems.dropper;
 
 public class DropperConstants {
     
+    public static Constraints kArmConstraints;
+    public static final double UPPER_LIMIT = 1;
+    public static final double LOWER_LIMIT = -1;
+
+    // add motor id's
 }
--- src/main/java/frc/robot/subsystems/dropper/DropperIO.java
+++ src/main/java/frc/robot/subsystems/dropper/DropperIO.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystems.dropper;
 
-public class DropperIO {
+public interface DropperIO {
+
+    public class ArmIOInputs {
+        public double angle;
+        public double velocity;
+        public double appliedVoltage;
+        public double angleRads;
+        public double angVelocityRadsPerSec;
+    }
     
+    public default void updateInputs(ArmIOInputs inputs) {}
+
+    public default double getAngle() {}
+
+    public default double getAngVelocity() {}
+
+    public default void setVoltage(double voltage) {}
+    
+    public default void setPosition(int position) {}
+    
+    public default void stop() {}
 }
