c6bd4afda196a5dca2af06939f7f50131cff96b1
EliasChang7 <eliaszychang@gmail.com>
2025-12-15 16:01:29 -0500

    redone io interface

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
 public class IntakeIOSim implements IntakeIO{
-    
+    private final FlywheelSim sim =
+      new FlywheelSim(
+          LinearSystemId.createFlywheelSystem(
+              DCMotor.getNEO(1), kMomentOfInertia, kAlgaeIntakeGearing),
+          DCMotor.getNEO(1));
+  private PIDController controller = new PIDController(0, 0, 0);
+
+  private double appliedVoltage = 0.0;
+
 }
