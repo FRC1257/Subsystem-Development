9ace06d6699b1605bc589ed431730b4c830f6c6a
EliasChang7 <eliaszychang@gmail.com>
2025-12-15 16:11:22 -0500

    asd

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,3 +1,19 @@
+package frc.robot.subsystems.Intake;
 public class IntakeConstants {
-    
+    public static class IntakeConstants{
+        public static final double kAlgaeIntakeP = 0.001;
+        public static final double kAlgaeIntakeI = 0.0;
+        public static final double kAlgaeIntakeD = 0.0;
+
+        public static final double kAlgaeIntakeGearing = 1.2;
+        public static final double kAlgaeIntakeDrumRadius = 0.03;
+        public static final double kCarriageMass = 0.15; // Mass in Kg
+        public static final double kMomentOfInertia =
+            0.5
+                * kCarriageMass
+                * kAlgaeIntakeDrumRadius
+                * kAlgaeIntakeDrumRadius; // Moment of inertia represents how resistant to force
+        // something is
+        
+    }
 }
