d88d0e0d42fc379aac28a743349da0fa69dfd53e
EliasChang7 <eliaszychang@gmail.com>
2025-12-15 16:28:37 -0500

    a lot of stuff

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -3,18 +3,12 @@ package frc.robot.subsystems.arm;
 import org.littletonrobotics.junction.AutoLog;
 
 public class ArmIO {
-    @AutoLog
-    public class ArmIOInputs{
+  @AutoLog
+  public class ArmIOInputs {}
 
-    }
+  public default void updateinputs(ArmIOInputs inputs) {}
 
-    public default void updateinputs(ArmIOInputs inputs){}
-    
-    public default void setVoltage(double volts){}
-    
-    public default void setPosition(double angle){}
+  public default void setVoltage(double volts) {}
 
-    
-
-
+  public default void setPosition(double angle) {}
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,11 +1,7 @@
 package frc.robot.subsystems.intake;
 
-import org.littletonrobotics.junction.AutoLog;
-
-
-
 public interface IntakeIO {
-    public static class IntakeIOInputs {
+  public static class IntakeIOInputs {
     public double velocityRadsPerSec = 0.0;
 
     public double appliedVoltage = 0.0;
@@ -23,13 +19,13 @@ public interface IntakeIO {
     return 0;
   }
 
-  public default void setDirection(boolean forward){}
-  
-  public default double getRPM(){
+  public default void setDirection(boolean forward) {}
+
+  public default double getRPM() {
     return 0;
   }
 
-  public default void setRPM(double rpm){}
-  
+  public default void setRPM(double rpm) {}
+
   public default void setPIDGains(double Kp, double Ki, double Kd) {}
 }
